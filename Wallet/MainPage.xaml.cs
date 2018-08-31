834044acd324de1f3542a5e7814ada828bbdf9ae
m.izdebski <m.izdebski@windowslive.com>
2018-08-31 21:02:07 +0200

    refactor: #7 move login logic to `login view`

--- Wallet/MainPage.xaml.cs
+++ Wallet/MainPage.xaml.cs
@@ -1,15 +1,11 @@
 ﻿using Autofac;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Wallet.Authorization;
-using Wallet.Common;
-using Wallet.Common.Helpers;
 using Wallet.Domain.Models;
 using Wallet.Domain.Services;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
@@ -34,45 +30,8 @@ namespace Wallet
         {
             this.InitializeComponent();
 
-            _httpClient = App.Container.Resolve<HttpClient>();
-            _authorization = App.Container.Resolve<IAuthorization>();
             _accountDataService = App.Container.Resolve<IAccountDataService>();
             _categoryService = App.Container.Resolve<ICategoryService>();
-
-            _localStorage = App.Container.Resolve<ILocalStorage>();
-
-
-            var apiKey = _localStorage.ReadVariableValue("ApiKey");
-#if DEBUG
-            if (apiKey == null)
-            {
-                Debug.WriteLine("ApiKey not stored.");
-
-                var loginParams = new List<KeyValuePair<string, string>>
-                {
-                    new KeyValuePair<string, string>("email", ""),
-                    new KeyValuePair<string, string>("password", "")
-                };
-
-                var uri = UriBuilderHelper.BuildUri(AccountAction.Session, ResponseType.Json);
-
-
-                Task.Run(() => _authorization.Authorize(_httpClient, loginParams, uri)).Wait();
-            }
-            else
-            {
-                Debug.WriteLine("ApiKey read from local storage.");
-            }
-
-
-
-#endif
-            Task.Run(async () =>
-            {
-                GroupedUserAccounts = await _accountDataService.GetGroupedUserAccounts();
-            }).Wait();
-
-            UserAccountsList.Source = GroupedUserAccounts;
         }
 
         private async void AccountsList_ItemClick(object sender, ItemClickEventArgs e)
@@ -85,6 +44,15 @@ namespace Wallet
             GroupedAccountTransactions = await _accountDataService.GetGroupedTransactionsByAccountId(selectedUserAccount.UserAccount.Id);
 
             AccountTransactionsList.Source = GroupedAccountTransactions;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            GroupedUserAccounts = await _accountDataService.GetGroupedUserAccounts();
+
+            UserAccountsList.Source = GroupedUserAccounts;
         }
     }
 }
--- Wallet/Views/LoginView.xaml.cs
+++ Wallet/Views/LoginView.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Windows.UI.Xaml;
+﻿using Autofac;
+using System.Collections.Generic;
+using System.Net.Http;
+using Wallet.Authorization;
+using Wallet.Common;
+using Wallet.Common.Helpers;
+using Wallet.Domain.Services;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=234238
@@ -10,13 +17,43 @@ namespace Wallet.Views
     /// </summary>
     public sealed partial class LoginView : Page
     {
+        private readonly HttpClient _httpClient;
+        private readonly IAuthorization _authorization;
+        private ILocalStorage _localStorage;
+
+        private object ApiKey { get; }
+
         public LoginView()
         {
             InitializeComponent();
+
+            _httpClient = App.Container.Resolve<HttpClient>();
+            _authorization = App.Container.Resolve<IAuthorization>();
+            _localStorage = App.Container.Resolve<ILocalStorage>();
+
+            ApiKey = _localStorage.ReadVariableValue("ApiKey");
         }
 
-        private void BtnLogIn_Click(object sender, RoutedEventArgs e)
+        private void Grid_Loading(FrameworkElement sender, object args)
         {
+            if (ApiKey != null)
+            {
+                Frame.Navigate(typeof(MainPage), ApiKey);
+            }
+        }
+
+        private async void BtnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            var loginParams = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("email", TxtUserName.Text),
+                    new KeyValuePair<string, string>("password", TxtPassword.Password)
+                };
+
+            var uri = UriBuilderHelper.BuildUri(AccountAction.Session, ResponseType.Json);
+
+            await _authorization.Authorize(_httpClient, loginParams, uri);
+
             Frame.Navigate(typeof(MainPage));
         }
 
@@ -30,9 +67,6 @@ namespace Wallet.Views
             BtnLogIn.IsEnabled = IsFormValid();
         }
 
-        private bool IsFormValid()
-        {
-            return TxtUserName.IsValid && TxtPassword.Password.Length > 0;
-        }
+        private bool IsFormValid() => TxtUserName.IsValid && TxtPassword.Password.Length > 0;
     }
 }
